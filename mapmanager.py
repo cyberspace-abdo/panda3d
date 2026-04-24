c7efb29c5ab39e33e2eccf3a6a484cca0a3249dd
cyberspace-abdo <soubaaabderrahmane@gmail.com>
2026-04-24 17:24:21 +0200

    Update mapmanager.py

--- mapmanager.py
+++ mapmanager.py
@@ -1,5 +1,20 @@
 class Mapmanager:
     def __init__(self):
-        self.model = 'blocky.egg'
-        self.texture = 'block.png'
-        self.color = (0.2, 0.2, 0.25, 1)
+        self.color = (0.2, 0.8, 0.2, 1)
+
+        self.startNew()
+        self.addBlock((0, 10, 0))
+
+    def startNew(self):
+        self.land = render.attachNewNode("Land")
+
+    def addBlock(self, position):
+        self.block = loader.loadModel("models/box")
+        self.block.setPos(position)
+        self.block.setScale(1)
+        self.block.setColor(self.color)
+        self.block.reparentTo(self.land)
+    def createMap(self):
+        for x in range(5):
+            for y in range(5):
+                self.addBlock((x, y + 10, 0))
