8a6cc95668bffad9bf6edb81d72d0c3a7102f33e
cyberspace-abdo <soubaaabderrahmane@gmail.com>
2026-04-24 17:24:02 +0200

    Fix class instantiation for Mapmanager and update game run

--- game.py
+++ game.py
@@ -3,8 +3,8 @@ from mapmanager import Mapmanager
 class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-
-        self.land = mapmanager()
+        self.land = Mapmanager()
         base.camLens.setFov(90)
-base = Game()
-base.run()
+
+game = Game()
+game.run()
